40b241d15265b9846c9d40cfdc93600da9dda465
Dawid Pach <polooo2@hotmail.com>
2015-01-06 15:06:32 +0100

    adds a test for undefined language

--- test/sonar_runner_test.js
+++ test/sonar_runner_test.js
@@ -23,6 +23,10 @@ describe('Sonar Runner', function () {
     it('default encoding is UTF-8', function () {
         sonarConfig['sonar.sourceEncoding'].should.be.equal('UTF-8');
     });
+    
+    it('default language is multi language (not set)', function () {
+        should.not.exist(sonarConfig['sonar.language']);
+    });
 
     it('database config is picked from jdbc node', function () {
         sonarConfig['sonar.jdbc.url'].should.be.equal('jdbc:mysql://localhost:3306/sonar');
