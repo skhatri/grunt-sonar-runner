ff103c415bdb1005961baf740da1245b91f7218e
Peter Janes <peter.janes@cineplex.com>
2016-05-02 10:51:09 -0400

    Fix tests

--- test/sonar_runner_test.js
+++ test/sonar_runner_test.js
@@ -7,7 +7,7 @@ describe('Sonar Runner', function () {
     var sonarConfig;
 
     beforeEach(function () {
-        var config = grunt.file.read('sonar-scanner-2.6/conf/sonar-runner.properties');
+        var config = grunt.file.read('sonar-scanner-2.6/conf/sonar-scanner.properties');
         var lines = config.split('\n');
         sonarConfig = Object.create(null);
         lines.forEach(function (line) {
