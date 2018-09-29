392b65783d6bfab6713b1402fd4b76f64a1b1b83
Chris Barry <chris@barry.im>
2018-09-29 12:20:02 -0400

    Issue #33 : Move to Sonar Scanner 3.x

--- test/sonar_runner_test.js
+++ test/sonar_runner_test.js
@@ -7,7 +7,7 @@ describe('Sonar Runner', function () {
     var sonarConfig;
 
     beforeEach(function () {
-        var config = grunt.file.read('sonar-runner-2.4/conf/sonar-runner.properties');
+        var config = grunt.file.read('sonar-scanner-3.2.0.1227/conf/sonar-scanner.properties');
         var lines = config.split('\n');
         sonarConfig = Object.create(null);
         lines.forEach(function (line) {
